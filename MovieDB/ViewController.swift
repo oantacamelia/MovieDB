010d08d1730c7d8d79ba54d4c500d1c7662a26fd
Camelia Oanta <cameliao@cameliao-mbp.local>
2019-02-08 13:51:04 +0200

    Initial Commit

---
+++ MovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by Camelia Oanta on 08/02/2019.
+//  Copyright © 2019 Camelia Oanta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
